44971458ead86cf8f6e72da8cb9b6db00eb38edd
Alonso-Pablo <sidokunoice@gmail.com>
2021-08-14 22:23:48 -0300

    PWA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 const withPlugins = require('next-compose-plugins')
 const withBundleAnalyzer = require('@next/bundle-analyzer')
 const withTM = require('next-transpile-modules')
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
-module.exports = withPlugins(
-  [withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' }), withTM([])],
-  { reactStrictMode: true }
-)
+module.exports =
+  (withPlugins(
+    [
+      withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' }),
+      withTM([])
+    ],
+    { reactStrictMode: true }
+  ),
+  withPWA({
+    pwa: {
+      dest: 'public',
+      runtimeCaching
+    }
+  }))
