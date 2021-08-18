b2f7858ddf82cbfc59fb0becf8bc33ff864bb1ec
Alonso-Pablo <sidokunoice@gmail.com>
2021-08-18 16:45:34 -0300

    Added metas and better UI

--- src/components/sections/TaskList.tsx
+++ src/components/sections/TaskList.tsx
@@ -120,7 +120,22 @@ const TaskList: React.FC<TaskListProps> = ({
                     cursor: isDragged ? 'grabbing' : 'grab'
                   }}
                   tabIndex={-1}
-                ></button>
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    height="20"
+                    viewBox="0 0 10 20"
+                    fill="none"
+                  >
+                    <rect y="8" width="4" height="4" rx="2" />
+                    <rect y="16" width="4" height="4" rx="2" />
+                    <rect width="4" height="4" rx="2" />
+                    <rect x="6" y="8" width="4" height="4" rx="2" />
+                    <rect x="6" y="16" width="4" height="4" rx="2" />
+                    <rect x="6" width="4" height="4" rx="2" />
+                  </svg>
+                </button>
                 <div
                   onClick={() => {
                     const { key = 0 } = props
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,15 +25,26 @@ class MyDocument extends Document {
             property="twitter:url"
             content="https://pomo-do-more.vercel.app/"
           />
+          <meta property="og:site_name" content="Pomo Do More"></meta>
           <meta property="twitter:title" content="&#127813; Pomo Do More" />
+          <meta property="og:title" content="&#127813; Pomo Do More" />
           <meta
             property="twitter:description"
             content="A minimalist Pomodoro clock for do more."
           />
           <meta
+            property="og:description"
+            content="A minimalist Pomodoro clock for do more."
+          ></meta>
+          <meta
             property="twitter:image:src"
             content="https://raw.githubusercontent.com/Alonso-Pablo/pomo-do-more/master/public/og.png"
           />
+          <meta
+            property="og:image"
+            content="https://raw.githubusercontent.com/Alonso-Pablo/pomo-do-more/master/public/og.png"
+          />
+          <meta property="og:url" content="https://pomo-do-more.vercel.app" />
         </Head>
         <body>
           <Main />
