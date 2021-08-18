933dc2dda45ed2af7b48d1f69e52968ef34dac88
Alonso-Pablo <sidokunoice@gmail.com>
2021-08-18 18:15:30 -0300

    Change meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,31 +20,36 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#D35A44" />
           <link rel="apple-touch-icon" href="/icon-76x76.png" />
 
-          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:title" content="&#127813; Pomo Do More" />
           <meta
             property="twitter:url"
             content="https://pomo-do-more.vercel.app/"
           />
-          <meta property="og:site_name" content="Pomo Do More"></meta>
-          <meta property="twitter:title" content="&#127813; Pomo Do More" />
-          <meta property="og:title" content="&#127813; Pomo Do More" />
           <meta
             property="twitter:description"
             content="A minimalist Pomodoro clock for do more."
           />
           <meta
-            property="og:description"
-            content="A minimalist Pomodoro clock for do more."
-          ></meta>
-          <meta
             property="twitter:image:src"
             content="https://raw.githubusercontent.com/Alonso-Pablo/pomo-do-more/master/public/og.png"
           />
+          <meta property="twitter:card" content="summary_large_image" />
+
+          <meta property="og:site_name" content="Pomo Do More" />
+          <meta property="og:title" content="&#127813; Pomo Do More" />
+          <meta
+            property="og:description"
+            content="A minimalist Pomodoro clock for do more."
+          />
+          <meta property="og:type" content="website" />
           <meta
             property="og:image"
+            itemProp="image"
             content="https://raw.githubusercontent.com/Alonso-Pablo/pomo-do-more/master/public/og.png"
           />
-          <meta property="og:url" content="https://pomo-do-more.vercel.app" />
+          <meta property="og:image:width" content="300" />
+          <meta property="og:image:height" content="300" />
+          <meta property="og:url" content="https://pomo-do-more.vercel.app/" />
         </Head>
         <body>
           <Main />
